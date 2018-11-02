1e5f1edfe694f1a3c6740f23ef606211faa21a73
root <jayfire1@gmail.com>
2018-11-02 12:58:31 -0400

    problem21 added

---
+++ Euler_21.py
@@ -0,0 +1,26 @@
+import math
+
+def sumdiv(n):
+    divs = 0
+
+    for i in range(1,int(math.sqrt(n))):
+        if n % i == 0:
+            divs += i
+
+    return divs
+
+if __name__ == "__main__":
+
+    amicable = set()
+    test = 0
+    maximum = int(input("Input the upper bound for the amicable numbers: "))
+
+    for i in range(1,maximum):
+        divi = sumdiv(i)
+        for j in range(i+1,maximum):
+            if divi == sumdiv(j):
+                amicable.add(i)
+                amicable.add(j)
+
+
+    print("The sum of all amicable numbers under {} is {}".format(maximum, sum(amicable)))
