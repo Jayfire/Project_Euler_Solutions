c15b273530a66d8c2663714a44bcb604eb4aaa8a
root <jayfire1@gmail.com>
2018-11-05 20:16:25 -0500

    Fixed divisor sum finding for 21 and finished 23

--- Euler_21.py
+++ Euler_21.py
@@ -6,14 +6,14 @@ def sumdiv(n):
     for i in range(1,int(math.sqrt(n))):
         if n % i == 0:
             divs += i
-            divs += n / i
+            if i != 1:
+                divs += n / i
 
     return divs
 
 if __name__ == "__main__":
 
     amicable = set()
-    test = 0
     maximum = int(input("Input the upper bound for the amicable numbers: "))
 
     for i in range(1,maximum):
--- Euler_23.py
+++ Euler_23.py
@@ -6,10 +6,30 @@ def sumdiv(n):
     for i in range(1,int(math.sqrt(n))):
         if n % i == 0:
             divs += i
-            divs += n / i
+            if i != 1:
+                divs += n / i
 
     return divs
 
 if __name__ == "__main__":
 
-    
+    abundant = set()
+    formable = set()
+    summed = 0
+    maximum = 28123
+
+    for i in range(12,maximum):
+        divi = sumdiv(i)
+        if divi > i:
+            abundant.add(i)
+
+    for i in abundant:
+        for j in abundant:
+            formable.add(i+j)
+
+    for i in range(28123):
+        if i not in formable:
+            summed += i
+
+
+    print("The sum of all numbers that are not the sum of two abundant numbers is {}".format(summed))
